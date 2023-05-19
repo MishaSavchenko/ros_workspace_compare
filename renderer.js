0aa2847c9041e4dfee8396592233493133398188
Misha Savchenko <savchenkmisha@gmail.com>
2023-05-18 19:32:05 -0500

    removed unnecessary function from the renderer

--- renderer.js
+++ renderer.js
@@ -1,17 +1,3 @@
-const { collapsable_callback } = require('./lib.js');
-
-document.getElementById('dirs').addEventListener('click', () => {
-    window.postMessage({
-        type: 'select-dirs'
-    })
-})
-
-var coll = document.getElementsByClassName("collapsible");
-for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", collapsable_callback);
-}
-
-
 document.getElementById('ws_comp').addEventListener('click', () => {
     window.postMessage({
         type: 'ws-comp'
